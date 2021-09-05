adf85d4f6d937a9faefa9582d6870bd04b75cdf3
Maxim323 <edi.dinu13@gmail.com>
2021-09-05 14:45:42 +0300

    Added instructions.

--- main.py
+++ main.py
@@ -3,164 +3,211 @@ import urllib.request
 import time
 start_time = time.time()
 
+#get the prefilled link from the google forms site
+#answer all the questions and generate the link
+#bind the variables in the link and Have fun!
+#Replace /viewform from the google link with /formResponse
+
+
+
+
 def ping():
-    var1 = ['Da']
-    var2 = [
-        'Heterosexual%C4%83+(atrac%C8%9Bie+numai+fa%C8%9B%C4%83+de+persoane+de+sexul+opus)',
-        'Homosexual%C4%83+(atrac%C8%9Bie+numai+fa%C8%9B%C4%83+de+persoanele+de+acela%C8%99i+sex)',
-        'Bisexual%C4%83+(atrac%C8%9Bie+fa%C8%9B%C4%83+de+ambele+sexe)',
-        'Asexual%C4%83+(nu+are+atrac%C8%9Bie+sexual%C4%83+fa%C8%9B%C4%83+de+niciun+sex+sau+gen).'
-    ]
-
-    var3 = [
-
-        'Op%C8%9Biunea+B',
-        'Op%C8%9Biunea+A'
-    ]
 
 
 
-    var14 = ['1', '2', '3', '4', '5']
-    var15 = ['1', '2', '3', '4', '5']
-    var16 = ['1', '2', '3', '4', '5']
-    var17 = ['1', '2', '3', '4', '5']
-    var18 = ['1', '2', '3', '4', '5']
-    var19 = ['1', '2', '3', '4', '5']
-    var20 = ['1', '2', '3', '4', '5']
-    var21 = ['1', '2', '3', '4', '5']
-    var22 = ['1', '2', '3', '4', '5']
-    var23 = ['1', '2', '3', '4', '5']
-    var24 = ['1', '2', '3', '4', '5']
-    var25 = ['1', '2', '3', '4', '5']
-    var26 = ['1', '2', '3', '4', '5']
-    var27 = ['1', '2', '3', '4', '5']
-    var28 = ['1', '2', '3', '4', '5']
-    var29 = ['1', '2', '3', '4', '5']
-    var30 = ['1', '2', '3', '4', '5']
-    var31 = ['1', '2', '3', '4', '5']
-    var32 = ['1', '2', '3', '4', '5']
-    var33 = ['1', '2', '3', '4', '5']
-    var34 = ['1', '2', '3', '4', '5']
-    var35 = ['1', '2', '3', '4', '5']
-    var36 = ['1', '2', '3', '4', '5']
-    var37 = ['1', '2', '3', '4', '5']
-    var38 = ['1', '2', '3', '4', '5']
-    var39 = ['1', '2', '3', '4', '5']
-    var40 = ['1', '2', '3', '4', '5']
-    var41 = ['1', '2', '3', '4', '5']
-    var42 = ['1', '2', '3', '4', '5']
-    var43 = ['1', '2', '3', '4', '5']
-    var44 = ['1', '2', '3', '4', '5']
-    var45 = ['1', '2', '3', '4', '5']
-    var46 = ['1', '2', '3', '4', '5']
-    var47 = ['1', '2', '3', '4', '5']
-    var48 = ['1', '2', '3', '4', '5']
-    var49 = ['1', '2', '3', '4', '5']
-    var50 = ['1', '2', '3', '4', '5']
-    var51 = ['1', '2', '3', '4', '5']
-    var52 = ['1', '2', '3', '4', '5']
-    var53 = ['1', '2', '3', '4', '5']
-    var54 = ['1', '2', '3', '4', '5']
-    var55 = ['1', '2', '3', '4', '5']
-    var56 = ['1', '2', '3', '4', '5']
-    var57 = ['1', '2', '3', '4', '5']
-    var58 = ['1', '2', '3', '4', '5']
-    var59 = ['1', '2', '3', '4', '5']
-    var60 = ['Angajat']
-    var61 = ['1', '2', '3', '4', '5']
-    var62 = ['1', '2', '3', '4', '5']
-    var63 = ['1', '2', '3', '4', '5']
-    var64 = ['1', '2', '3', '4', '5']
-    var65 = ['1', '2', '3', '4', '5']
-    var66 = ['1', '2', '3', '4', '5']
-    var67 = ['1', '2', '3', '4', '5']
-    var68 = ['1', '2', '3', '4', '5']
-    var69 = ['1', '2', '3', '4', '5']
-    var70 = ['1', '2', '3', '4', '5']
-    var71 = ['1', '2', '3', '4', '5']
-    var72 = ['1', '2', '3', '4', '5']
-    var73 = ['1', '2', '3', '4', '5']
-    var74 = ['1', '2', '3', '4', '5']
-    var75 = ['1', '2', '3', '4', '5']
-    var76 = ['1', '2', '3', '4', '5']
-    var77 = ['1', '2', '3', '4', '5']
-    var78 = ['1', '2', '3', '4', '5']
-    var79 = ['1', '2', '3', '4', '5']
-    var80 = ['1', '2', '3', '4', '5']
 
-    link = "https://docs.google.com/forms/d/e/1FAIpQLSdajkC2Y9bgcjdL4OCwwvvpx9W1vM10ApiPA0t6pHKo__-_OA/formResponse?usp=pp_url&entry.2098263493=%s&entry.1497167764=%s&entry.1495890103=%s&&entry.878824247=%s&entry.191527542=%s&entry.1249069091=%s&entry.1948437240=%s&entry.2101716554=%s&entry.459684729=%s&entry.738055451=%s&entry.1157299153=%s&entry.1676283875=%s&entry.149184897=%s&entry.1387901439=%s&entry.369864373=%s&entry.34781371=%s&entry.1634826413=%s&entry.116547836=%s&entry.633538413=%s&entry.1301739020=%s&entry.1116985856=%s&entry.1298171401=%s&entry.1048195479=%s&entry.11921290=%s&entry.861289069=%s&entry.552270372=%s&entry.1428212545=%s&entry.1254648214=%s&entry.1376975919=%s&entry.927374066=%s&entry.502207017=%s&entry.665783085=%s&entry.1958877850=%s&entry.1115074390=%s&entry.1148898843=%s&entry.254418=%s&entry.1666792128=%s&entry.1576477473=%s&entry.669154977=%s&entry.1539534802=%s&entry.1324829331=%s&entry.761830038=%s&entry.1938866727=%s&entry.1954765330=%s&entry.682436656=%s&entry.1702714142=%s&entry.184950505=%s&entry.559718648=%s&entry.1173716465=%s&entry.554365494=%s&entry.485271122=%s&entry.1697032384=%s&entry.1065944024=%s&entry.1491744212=%s&entry.902310139=%s&entry.1426285537=%s&entry.1243065005=%s&entry.236542799=%s&entry.92052687=%s&entry.809913012=%s&entry.1206683461=%s&entry.805607913=%s&entry.1277348542=%s&entry.537863305=%s&entry.59428512=%s&entry.1007406158=%s&entry.960706476=%s&entry.2010185824=%s&entry.1335994241=%s&entry.326540379=%s"\
-        % \
-        (random.choice(var1),
-         random.choice(var2),
-         random.choice(var3),
-         random.choice(var14),
-         random.choice(var15),
-         random.choice(var16),
-         random.choice(var17),
-         random.choice(var18),
-         random.choice(var19),
-         random.choice(var20),
-         random.choice(var21),
-         random.choice(var22),
-         random.choice(var23),
-         random.choice(var24),
-         random.choice(var25),
-         random.choice(var26),
-         random.choice(var27),
-         random.choice(var28),
-         random.choice(var29),
-         random.choice(var30),
-         random.choice(var31),
-         random.choice(var32),
-         random.choice(var33),
-         random.choice(var34),
-         random.choice(var35),
-         random.choice(var36),
-         random.choice(var37),
-         random.choice(var38),
-         random.choice(var39),
-         random.choice(var40),
-         random.choice(var41),
-         random.choice(var42),
-         random.choice(var43),
-         random.choice(var44),
-         random.choice(var45),
-         random.choice(var46),
-         random.choice(var47),
-         random.choice(var48),
-         random.choice(var49),
-         random.choice(var50),
-         random.choice(var51),
-         random.choice(var52),
-         random.choice(var53),
-         random.choice(var54),
-         random.choice(var55),
-         random.choice(var56),
-         random.choice(var57),
-         random.choice(var58),
-         random.choice(var59),
-         random.choice(var60),
-         random.choice(var61),
-         random.choice(var62),
-         random.choice(var63),
-         random.choice(var64),
-         random.choice(var65),
-         random.choice(var66),
-         random.choice(var67),
-         random.choice(var68),
-         random.choice(var69),
-         random.choice(var70),
-         random.choice(var71),
-         random.choice(var72),
-         random.choice(var73),
-         random.choice(var74),
-         random.choice(var75),
-         random.choice(var76),
-         random.choice(var77),
-         random.choice(var78),
-         random.choice(var79),
-         random.choice(var80)
-    )
+    consent = 'Da'
+    sex = ['Masculin', 'Feminin']
+    program = ['Part-time', 'Full+time']
+    format = '%C3%AEn+format+hibrid'
+    #starecivila = 'nec%C4%83s%C4%83torit'
+    starecivila = 'c%C4%83s%C4%83torit'
+
+    var1 = ['1', '2', '3', '4']
+    var2 = ['1', '2', '3', '4']
+    var3 = ['1', '2', '3', '4']
+    var4 = ['1', '2', '3', '4']
+    var5 = ['1', '2', '3', '4']
+    var6 = ['1', '2', '3', '4']
+    var7 = ['1', '2', '3', '4']
+    var8 = ['1', '2', '3', '4']
+    var9 = ['1', '2', '3', '4']
+    var10 = ['1', '2', '3', '4']
+    var11 = ['1', '2', '3', '4']
+    var12 = ['1', '2', '3', '4']
+    var13 = ['1', '2', '3', '4']
+    var14 = ['1', '2', '3', '4']
+    var15 = ['1', '2', '3', '4']
+    var16 = ['1', '2', '3', '4']
+    var17 = ['1', '2', '3', '4']
+    var18 = ['1', '2', '3', '4']
+    var19 = ['1', '2', '3', '4']
+    var20 = ['1', '2', '3', '4']
+    var21 = ['1', '2', '3', '4']
+    var22 = ['1', '2', '3', '4']
+    var23 = ['1', '2', '3', '4']
+    var24 = ['1', '2', '3', '4']
+    var25 = ['1', '2', '3', '4']
+    var26 = ['1', '2', '3', '4']
+    var27 = ['1', '2', '3', '4']
+    var28 = ['1', '2', '3', '4']
+    var29 = ['1', '2', '3', '4']
+    var30 = ['1', '2', '3', '4']
+    var31 = ['1', '2', '3', '4']
+    var32 = ['1', '2', '3', '4']
+    var33 = ['1', '2', '3', '4']
+    var34 = ['1', '2', '3', '4']
+    var35 = ['1', '2', '3', '4']
+    var36 = ['1', '2', '3', '4']
+    var37 = ['1', '2', '3', '4']
+    var38 = ['1', '2', '3', '4']
+    var39 = ['1', '2', '3', '4']
+    var40 = ['1', '2', '3', '4']
+    var41 = ['1', '2', '3', '4']
+    var42 = ['1', '2', '3', '4']
+    var43 = ['1', '2', '3', '4']
+    var44 = ['1', '2', '3', '4']
+    var45 = ['1', '2', '3', '4']
+    var46 = ['1', '2', '3', '4']
+    var47 = ['1', '2', '3', '4']
+    var48 = ['1', '2', '3', '4']
+    var49 = ['1', '2', '3', '4']
+    var50 = ['1', '2', '3', '4']
+    var51 = ['1', '2', '3', '4']
+    var52 = ['1', '2', '3', '4']
+    var53 = ['1', '2', '3', '4']
+    var54 = ['1', '2', '3', '4']
+    var55 = ['1', '2', '3', '4']
+    var56 = ['1', '2', '3', '4']
+    var57 = ['1', '2', '3', '4']
+    var58 = ['1', '2', '3', '4']
+    var59 = ['1', '2', '3', '4']
+    var60 = ['1', '2', '3', '4']
+    var61 = ['1', '2', '3', '4']
+    var62 = ['1', '2', '3', '4']
+    var63 = ['1', '2', '3', '4']
+    var64 = ['1', '2', '3', '4']
+    var65 = ['1', '2', '3', '4']
+    var66 = ['1', '2', '3', '4']
+    var67 = ['1', '2', '3', '4']
+    var68 = ['1', '2', '3', '4']
+    var69 = ['1', '2', '3', '4']
+    var70 = ['1', '2', '3', '4']
+    var71 = ['1', '2', '3', '4']
+    var72 = ['1', '2', '3', '4']
+    var73 = ['1', '2', '3', '4']
+    var74 = ['1', '2', '3', '4']
+    var75 = ['1', '2', '3', '4']
+    var76 = ['1', '2', '3', '4']
+    var77 = ['1', '2', '3', '4']
+    var78 = ['1', '2', '3', '4']
+    var79 = ['1', '2', '3', '4']
+    var80 = ['1', '2', '3', '4']
+    var81 = ['1', '2', '3', '4']
+    var82 = ['1', '2', '3', '4']
+    var83 = ['1', '2', '3', '4']
+    var84 = ['1', '2', '3', '4']
+    var85 = ['1', '2', '3', '4']
+    var86 = ['1', '2', '3', '4']
+    var87 = ['1', '2', '3', '4']
+
+
+    link = "https://docs.google.com/forms/d/e/1FAIpQLSfEkcVQQgQH1CTKh88EdHMsQ9NgIHW6xWWshX65avyazhfUWg/formResponse?usp=pp_url&entry.1908071108=%s&entry.924631626=%s&entry.1581450637=%s&entry.1707537598=%s&entry.345679069=%s&entry.1854371412=%s&entry.1041098762=%s&entry.815264443=%s&entry.1554560101=%s&entry.723673990=%s&entry.1974101141=%s&entry.120265887=%s&entry.192105148=%s&entry.1757545852=%s&entry.1110410873=%s&entry.1679415238=%s&entry.457727738=%s&entry.1078371257=%s&entry.2032812765=%s&entry.1491152001=%s&entry.1205175046=%s&entry.1620973532=%s&entry.951255637=%s&entry.818437160=%s&entry.385655884=%s&entry.1343873096=%s&entry.205130525=%s&entry.2089865502=%s&entry.1761564209=%s&entry.1020142211=%s&entry.1685106764=%s&entry.704699746=%s&entry.488387515=%s&entry.1853410795=%s&entry.2015052017=%s&entry.908487546=%s&entry.702414003=%s&entry.416639818=%s&entry.96743223=%s&entry.1152972155=%s&entry.675170620=%s&entry.368942998=%s&entry.1996203051=%s&entry.250828938=%s&entry.624187205=%s&entry.6201814=%s&entry.1999051855=%s&entry.944907412=%s&entry.2058249500=%s&entry.1466512121=%s&entry.1358453516=%s&entry.1650619907=%s&entry.1030697073=%s&entry.877472777=%s&entry.1323040831=%s&entry.982839720=%s&entry.1187273439=%s&entry.134874649=%s&entry.178297728=%s&entry.568232674=%s&entry.447118686=%s&entry.1932327006=%s&entry.837705166=%s&entry.391768471=%s&entry.1483844214=%s&entry.641514195=%s&entry.64087912=%s&entry.257614867=%s&entry.233026474=%s&entry.30073468=%s&entry.1842271456=%s&entry.1298220809=%s&entry.624110224=%s&entry.1811319129=%s&entry.665164202=%s&entry.261599856=%s&entry.481513632=%s&entry.54671960=%s&entry.943325717=%s&entry.616350792=%s&entry.27259333=%s&entry.1435785036=%s&entry.912629462=%s&entry.2051253511=%s&entry.469678524=%s&entry.1203697997=%s&entry.1050380184=%s&entry.352647104=%s&entry.1618371355=%s&entry.1331848274=%s&entry.2139636153=%s&entry.755426511=%s" \
+           % \
+           (consent,
+            random.choice(sex),
+            random.choice(program),
+            format,
+            starecivila,
+            random.choice(var1),
+            random.choice(var2),
+            random.choice(var3),
+            random.choice(var4),
+            random.choice(var5),
+            random.choice(var6),
+            random.choice(var7),
+            random.choice(var8),
+            random.choice(var9),
+            random.choice(var10),
+            random.choice(var11),
+            random.choice(var12),
+            random.choice(var13),
+            random.choice(var14),
+            random.choice(var15),
+            random.choice(var16),
+            random.choice(var17),
+            random.choice(var18),
+            random.choice(var19),
+            random.choice(var20),
+            random.choice(var21),
+            random.choice(var22),
+            random.choice(var23),
+            random.choice(var24),
+            random.choice(var25),
+            random.choice(var26),
+            random.choice(var27),
+            random.choice(var28),
+            random.choice(var29),
+            random.choice(var30),
+            random.choice(var31),
+            random.choice(var32),
+            random.choice(var33),
+            random.choice(var34),
+            random.choice(var35),
+            random.choice(var36),
+            random.choice(var37),
+            random.choice(var38),
+            random.choice(var39),
+            random.choice(var40),
+            random.choice(var41),
+            random.choice(var42),
+            random.choice(var43),
+            random.choice(var44),
+            random.choice(var45),
+            random.choice(var46),
+            random.choice(var47),
+            random.choice(var48),
+            random.choice(var49),
+            random.choice(var50),
+            random.choice(var51),
+            random.choice(var52),
+            random.choice(var53),
+            random.choice(var54),
+            random.choice(var55),
+            random.choice(var56),
+            random.choice(var57),
+            random.choice(var58),
+            random.choice(var59),
+            random.choice(var60),
+            random.choice(var61),
+            random.choice(var62),
+            random.choice(var63),
+            random.choice(var64),
+            random.choice(var65),
+            random.choice(var66),
+            random.choice(var67),
+            random.choice(var68),
+            random.choice(var69),
+            random.choice(var70),
+            random.choice(var71),
+            random.choice(var72),
+            random.choice(var73),
+            random.choice(var74),
+            random.choice(var75),
+            random.choice(var76),
+            random.choice(var77),
+            random.choice(var78),
+            random.choice(var79),
+            random.choice(var80),
+            random.choice(var81),
+            random.choice(var82),
+            random.choice(var83),
+            random.choice(var84),
+            random.choice(var85),
+            random.choice(var86),
+            random.choice(var87),
+            )
+
 
     getSite = urllib.request.urlopen(link)
 
@@ -168,9 +215,10 @@ def ping():
 def loop(N):
     for x in range(0, N):
         ping()
+
         print("Done: %s" % x)
 
 
 # cate raspunsuri vrei
-loop(96)
+loop(5)
 print("Time while running: %s seconds." % (time.time() - start_time))
